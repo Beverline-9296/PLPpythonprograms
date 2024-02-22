d761ca21a19824fbe6b0dfc3a18cdcdcfd0d3947
beveline omondi <beverlineomondi827@gmail.com>
2024-02-22 06:54:50 +0300

    my first commit

---
+++ first.py
@@ -0,0 +1,11 @@
+#python program to print hello world
+print('Hello , World!!')
+#python program to assign same value to a multiple variables
+site1=site2="power learn project"
+print(site1)
+print(site2)
+a = (1, 2, 3)
+
+b = list(a)
+
+print(b)
