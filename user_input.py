bec7b2d172f64897607fa8af1e501824fc7c0e77
beveline omondi <beverlineomondi827@gmail.com>
2024-02-22 06:56:42 +0300

    person's detail

---
+++ user_input.py
@@ -0,0 +1,9 @@
+#a python program to prompt the user to enter their name,age and location
+#propt the user to enter her/his name
+name=(input("Enter your name:"))
+#prompt the user to enter the age
+age=int(input("Enter your age:"))
+#prompt the user to enter location
+location=input("Enter your location:")
+#to print the output after the user has enter their details
+print(f"Hello {name}, you are {age} years old and live in {location}.")
